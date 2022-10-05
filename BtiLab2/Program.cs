dd976500f422a5cc226f1881eae253df26a8ce36
makka999 <55660660+makka999@users.noreply.github.com>
2022-10-05 17:40:39 +0200

    koniec

--- BtiLab2/Program.cs
+++ BtiLab2/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 
 
-static string Encrypt(string text, char[] alphabet, string key)
+static string Encrypt(string text, char[] alphabet, string key, bool enEncrypt)
 {
+    int newIndex;
     text = text.ToLower();
     char[] textToEncrypt = text.ToCharArray();
     key = key.ToLower();
@@ -18,36 +19,8 @@ static string Encrypt(string text, char[] alphabet, string key)
             int index = Array.IndexOf(alphabet, letter);
             var letterKey = charKey[(i)%charKey.Length];
             int indexKey = Array.IndexOf(alphabet, letterKey);
-            int newIndex = (indexKey + index) % alphabetLenght;
-            if (newIndex < 0) newIndex = alphabetLenght + newIndex;
-            char newLetter = alphabet[newIndex];
-            encryptedChar[i] = newLetter;
-        }
-        else encryptedChar[i] = textToEncrypt[i];
-    }
-
-    string encrypToText = String.Join("", encryptedChar);
-    return encrypToText;
-}
-
-static string EnEncrypt(string text, char[] alphabet, string key)
-{
-    text = text.ToLower();
-    char[] textToEncrypt = text.ToCharArray();
-    key = key.ToLower();
-    char[] charKey = key.ToCharArray();
-    int alphabetLenght = alphabet.Length;
-    int length = textToEncrypt.Length;
-    char[] encryptedChar = new char[length];
-    for (int i = 0; i < textToEncrypt.Length; i++)
-    {
-        if (!(textToEncrypt[i] == ' '))
-        {
-            var letter = textToEncrypt[i];
-            int index = Array.IndexOf(alphabet, letter);
-            var letterKey = charKey[(i)%charKey.Length];
-            int indexKey = Array.IndexOf(alphabet, letterKey);
-            int newIndex = (index - indexKey) % alphabetLenght;
+            if (enEncrypt == false) newIndex = (indexKey + index) % alphabetLenght;
+            else  newIndex = (index - indexKey) % alphabetLenght;
             if (newIndex < 0) newIndex = alphabetLenght + newIndex;
             char newLetter = alphabet[newIndex];
             encryptedChar[i] = newLetter;
@@ -60,18 +33,24 @@ static string EnEncrypt(string text, char[] alphabet, string key)
 }
 
 
-char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 string text, result, key;
 
 text = "jedna z najprostszych technik szyfrowania";
 key = "jedna";
+Console.WriteLine();
+Console.WriteLine();
+
 Console.WriteLine(text);
 Console.WriteLine(key);
-result = Encrypt(text, alphabet, key);
+Console.WriteLine();
+result = Encrypt(text, alphabet, key, false);
+Console.WriteLine(result);
+Console.WriteLine();
+
+text = result;
+result = Encrypt(text, alphabet, key, true);
 Console.WriteLine(result);
 
-text = "sigaa d aastubscwclcq wrcqrlx bdbsrxadaij";
-result = Encrypt(text, alphabet, key);
-Console.WriteLine(result);
-result = EnEncrypt(text, alphabet, key);
-Console.WriteLine(result);
